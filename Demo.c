fd408d1bbd77ee4fa3770e59ca0e276953199da3
Ashutosh <ashuraj001rokade@gmail.com>
2022-06-14 14:58:50 +0530

    first commit

---
+++ Demo.c
@@ -0,0 +1,16 @@
+// program to add two numbers
+
+#include<stdio.h>
+
+int main()
+{
+ int iValue1 = 10;
+ int iValue2 = 20;
+int iSum = 0;
+iSum = iValue1+ iValue2;
+
+printf("%d", iSum);
+
+
+return 0 ;
+}
